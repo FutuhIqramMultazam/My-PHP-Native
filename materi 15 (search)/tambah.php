9b109d72be14ef2da37ad7d808cc2348643dc923
FutuhIqramMultazam <2307014@itg.ac.id>
2024-06-22 20:56:02 +0700

    ada kesalahan di pesan notifikasi tambah dan ubah, jadi if dan else nya sama pesan nya, harusnya kan satu berhasil dan satu gagal

--- materi 15 (search)/tambah.php
+++ materi 15 (search)/tambah.php
@@ -1,46 +1,48 @@
 <?php
 require 'functions.php';
 
-if (isset($_POST["kirim"])){
-    
+if (isset($_POST["kirim"])) {
+
     // cek data apakah berhasil di tambahkan atau tidak
-   if (tambah($_POST) > 0 ){
-    echo "<script>
+    if (tambah($_POST) > 0) {
+        echo "<script>
             alert('Data berhasil di tambahkan');
             document.location.href = 'index.php';    
         </script>";
-   } else{
-    echo "<script>
-    alert('Data berhasil di tambahkan');
+    } else {
+        echo "<script>
+    alert('Data gagal di tambahkan');
     document.location.href = 'index.php';    
     </script>";
-   }
-
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tambah data mahasiswa</title>
 </head>
+
 <body>
     <h1>Tambah Data Mahasiswa</h1>
 
     <form action="" method="post">
         <ul>
-            <li><input type="text" name="nim" placeholder="NIM:" required ></li>
-            <li><input type="text" name="nama" placeholder="Nama:" required ></li>
+            <li><input type="text" name="nim" placeholder="NIM:" required></li>
+            <li><input type="text" name="nama" placeholder="Nama:" required></li>
             <li><input type="text" name="email" placeholder="email:"></li>
             <li><input type="text" name="jurusan" placeholder="Jurusan:"></li>
             <li><input type="text" name="gambar" placeholder="Gambar:"></li>
-            <li><button type="submit" name="kirim" >Tambah Data!</button></li>
+            <li><button type="submit" name="kirim">Tambah Data!</button></li>
         </ul>
     </form>
 
     <a href="index.php">kembali ke daftar mahasiswa</a>
 
 </body>
+
 </html>
