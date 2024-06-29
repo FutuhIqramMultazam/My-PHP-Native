a530677630e1e53aadd224850da875bbc69b16b7
FutuhIqramMultazam <2307014@itg.ac.id>
2024-06-29 15:58:05 +0700

    ada kesalahan ketika insert query, seharusnya sebelum values itu mendeklarasi atau memberitahu kepada mysqli apa saja yang akan di insert, harusnya seperti ini "mahasiswa (nama, nim) VALUES ('','');"

--- materi 13 (insert & delete)/functions.php
+++ materi 13 (insert & delete)/functions.php
@@ -1,18 +1,20 @@
-<?php 
+<?php
 // menghubungkan ke database
 $conn = mysqli_connect("localhost", "root", "", "phpdasar");
 
-function query( $query ) {
+function query($query)
+{
     global $conn;
-    $result = mysqli_query( $conn, $query );
-    $rows=[];
-    while($row = mysqli_fetch_assoc( $result )){
-        $rows[]=$row;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
     }
-    return $rows; 
+    return $rows;
 }
 
-function tambah ( $data ) {
+function tambah($data)
+{
     global $conn;
     // ambil data dari tiap elemen dalam form
     $nim = htmlspecialchars($data["nim"]); // htmlspecialchars berfungsi untuk menangkal orang jail ketika memasukan input di form yang aneh aneh
@@ -22,18 +24,16 @@ function tambah ( $data ) {
     $gambar = htmlspecialchars($data["gambar"]);
 
     // query insert data
-    $query = "INSERT INTO mahasiswa
-                VALUES
-            ('', '$nama', '$nim', '$email', '$jurusan', '$gambar')
-            ";
+    $query = "INSERT INTO mahasiswa (nama,nim,email,jurusan,gambar) VALUES ('$nama', '$nim', '$email', '$jurusan', '$gambar')"; // ada kesalahan ketika insert query, seharusnya sebelum values itu mendeklarasi atau memberitahu kepada mysqli apa saja yang akan di insert, harusnya seperti ini "mahasiswa (nama, nim) VALUES ('$nama',''$nim); 
+
     mysqli_query($conn, $query);
 
     return mysqli_affected_rows($conn);
 }
 
-function hapus ($id) {
+function hapus($id)
+{
     global $conn;
-    mysqli_query( $conn,"DELETE FROM mahasiswa WHERE id = $id");
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
     return mysqli_affected_rows($conn);
 }
-?>
