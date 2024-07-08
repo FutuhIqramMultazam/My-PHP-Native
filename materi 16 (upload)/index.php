b09ac423094a8044a075ae0b1c93849555cd2b52
FutuhIqramMultazam <2307014@itg.ac.id>
2024-07-09 00:17:49 +0700

    materi 16 upload

--- materi 16 (upload)/index.php
+++ materi 16 (upload)/index.php
@@ -12,12 +12,13 @@ if (isset($_POST['cari'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Daftar mahasiswa</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
   <style>
     /* body{
             background-color: black;
@@ -52,18 +53,18 @@ if (isset($_POST['cari'])) {
 <body>
   <div class="konten">
     <h1>Daftar Mahasiswa</h1>
-    <a href="tambah.php">tambah data mahasiswa</a>
+    <a class="btn btn-primary " href="tambah.php">tambah data mahasiswa</a>
     <br><br>
 
     <form action="" method="post">
-      <input type="text" name="keyword" autofocus autocomplete="off" placeholder="ketikan untuk mencari data di sini" size="30">
-      <button type="submit" name="cari">Cari data</button>
+      <input type="text" name="keyword" autofocus autocomplete="off" placeholder="ketikan untuk mencari data di sini" size="30" class="form-control w-25 d-inline">
+      <button type="submit" name="cari" class="btn btn-outline-success">Cari data</button>
     </form>
     <br>
 
-    <div class="tabel">
-      <table border="1" cellpadding="10" cellspacing="0">
-        <tr class="atas">
+    <div class="tabel ">
+      <table class="table table-borderless">
+        <tr class="atas text-black">
           <th>No.</th>
           <th>Aksi</th>
           <th>Gambar</th>
@@ -77,8 +78,8 @@ if (isset($_POST['cari'])) {
           <tr>
             <td><?= $i ?></td>
             <td>
-              <a href="ubah.php?id=<?= $row["id"]; ?>">Ubah |</a>
-              <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');"> Hapus</a>
+              <a class="btn btn-info btn-sm" href="ubah.php?id=<?= $row["id"]; ?>">Ubah </a> |
+              <a class="btn btn-danger btn-sm" href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');"> Hapus</a>
             </td>
             <td><img src="img/<?= $row["gambar"]; ?>" width="40px"></td>
             <td><?= $row["nim"]; ?></td>
