b173ee021651196f694a9dd857e45f8795a48b63
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-07-22 17:14:23 +0700

    menambahkan keamanan di bagian fungsi registrasi

--- materi 17 (registrasi)/functions.php
+++ materi 17 (registrasi)/functions.php
@@ -136,9 +136,22 @@ function registrasi($data)
 {
     global $conn;
 
-    $username = strtolower(stripslashes($data['username']));
-    $password = mysqli_real_escape_string($conn, $data['password']);
-    $password2 = mysqli_real_escape_string($conn, $data['password2']);
+    $username = htmlspecialchars(strtolower(stripslashes($data['username'])));
+    $password = htmlspecialchars(mysqli_real_escape_string($conn, $data['password']));
+    $password2 = htmlspecialchars(mysqli_real_escape_string($conn, $data['password2']));
+
+    if (empty($username)) {
+        echo "<script>
+            alert('Username harus di isi');
+            </script>";
+        return false;
+    }
+    if (empty($password)) {
+        echo "<script>
+            alert('Username harus di isi');
+            </script>";
+        return false;
+    }
 
     // cek konfirmasi username suadah ada atau belum 
     $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
