a2c5993c8d4e1d3f7ace9103d77c2dbdf7464a09
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-07-25 20:28:03 +0700

    samting

--- materi 19 (SESSION)/session/halaman2.php
+++ materi 19 (SESSION)/session/halaman2.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
-echo "Ini hasil dari superglobal variable session = ", $_SESSION["nama"];
+// echo "Ini hasil dari superglobal variable session = ", $_SESSION["nama"];
+
+if (isset($_SESSION["nama"])) {
+    echo "variable ada isinya: ", $_SESSION["nama"];
+} else {
+    echo "variable tidak ada isinya";
+}
 
 echo "<br><a href='halaman1.php'>kembali</a>";
