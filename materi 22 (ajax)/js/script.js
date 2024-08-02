3f4ae270027462371c9db6b1abecefd53f930fb0
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-08-02 20:08:11 +0700

    menambahkan folder untuk materi ajax dan menambahkan folder js untuk menyimpan script ajax

---
+++ materi 22 (ajax)/js/script.js
@@ -0,0 +1,12 @@
+// ambil element
+// var cari = document.getElementById("");
+// var tombol = document.getElementById("");
+// var tabel = document.getElementById("");
+
+// //tambah event
+
+// cari.addEventListener("keyup", function () {
+//   console.log(cari.value);
+// });
+
+console.log("ok");
